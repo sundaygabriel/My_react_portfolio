18091575ef94c2add2759ee7981e956d6f6d673a
jeremiahalao <jeremiahalao231@gmail.com>
2022-08-05 12:29:01 +0100

    Portfolio in React

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class ContactUs extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="contact">
-          <div className="row section-head">
-            <div className="ten columns">
-              <p className="lead">
-              Feel free to contact me for any work or suggestions below
-              </p>
+        <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">
+              <span style={{color : "red"}}>
+                Its a Great privillage for me to be amoung the Pwc Tech BootCamp
+                Trainee Particpant
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <h4>Linked in :{resumeData.linkedinId}</h4>
+              <h4>Facebookid in :{resumeData.Facebookid}</h4>
+              <h4>Email in :{resumeData.Email}</h4>
             </div>
-          </div>
-          <div className="row">
-            <aside className="eigth columns footer-widgets">
-              <div className="widget">
-                <h4>Linked in :
-                  {resumeData.linkedinId}
-                </h4>
-              </div>
-            </aside>
-          </div>
-        </section>
-        );
+          </aside>
+        </div>
+      </section>
+    );
   }
 }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,83 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Jeremiah Alao",
+  role: "Pwc Tech BootCamp Trainee ",
+  linkedinId: "jerrylawn",
+  Facebookid: "Jeremiah Oluwagbemi",
+  Email: "Alaojeremiah@gmail.com",
+
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/alao-j-b28b9367/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/jerrylawn",
+      className: "fa fa-github",
+    },
+
+    {
+      name: "Whatapp",
+      url: "+2349036368054",
+      className: "fa fa-whatsapp",
+    },
+    {
+      name: "Facebook",
+      url: "https://wwww.facebook.com/alaojerryboy",
+      className: "fa fa-facebook",
+    },
+    {
+      name: "Gmail",
+      url: "alaojeremiah@gmail.com",
+      className: "fa fa-gmail",
+    },
+  ],
+  aboutme:
+    "I am a tenacious, teachable, and responsible individual, able to adapt and thrive in a fast paced and dynamic environment. I am seeking a position in an innovative, ethical, and stable organization with clear opportunity for learning and career advancement. I am always eager to learn, share my knowledge and, a great team player with experience in system administrator, Software developer and Cybersecurity",
+  address: "No 11, Adisa Coker Street Obawole , Ifako, Lagos, State",
+  Tell: "+2349036368054",
+  email: "Alaojeremiah@gmail.com",
+  education: [
+    {
+      UniversityName: "Federal University of Technology Minna",
+      specialization: "Automobile Technology",
+      MonthOfPassing: "Nov",
+      YearOfPassing: "2019",
+      Degree: "B,Tech",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Laralek Ultimate Limited",
+      specialization: "IT Officer",
+      MonthOfLeaving: "Oct",
+      YearOfLeaving: "2021",
+    },
+    {
+      CompanyName: "Pwc Tech Bootcamp ",
+      specialization: "Full Stack Software Developer",
+      MonthOfLeaving: "july",
+      YearOfLeaving: "2022",
+    },
+  ],
+  skillsDescription: "Cybersecurity, Software Developer",
+  skills: [
+   
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "JAVASCRIPT",
+    },
+    {
+      skillname: "REACTJS",
+    },
+  ],
+};
+
+export default resumeData;
